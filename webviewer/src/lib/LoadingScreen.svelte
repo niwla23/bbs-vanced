c24d27df111a905d2ef3d91069d855e973b38f01
niwla23 <46248939+niwla23@users.noreply.github.com>
2023-08-21 22:30:54 +0200

    ahhhh we are stuck in a loading loop

--- webviewer/src/lib/LoadingScreen.svelte
+++ webviewer/src/lib/LoadingScreen.svelte
@@ -10,8 +10,8 @@
 	<div class="text-center flex flex-col items-center">
 		<p class="text-[10rem] md:text-[14rem] animate-spin text-center w-min">😱</p>
 		<p>Ich suche deinen Stundenplan...</p>
-		<small class="text-xs text-gray-300"
-			>(Dieser Loader wurde in 5 Minuten um 3 Uhr nachts gebaut)</small
-		>
+		<small class="text-xs text-gray-300">
+			(Dieser Loader wurde in 5 Minuten um 3 Uhr nachts gebaut)
+		</small>
 	</div>
 </div>
