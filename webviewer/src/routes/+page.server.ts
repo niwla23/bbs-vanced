1e790442b3896a76fc027f44b22d542910b03fff
niwla23 <46248939+niwla23@users.noreply.github.com>
2023-08-21 22:22:08 +0200

    always open correct day

--- webviewer/src/routes/+page.server.ts
+++ webviewer/src/routes/+page.server.ts
@@ -1,7 +1,32 @@
 import { redirect } from '@sveltejs/kit';
 
+// black magic, stole it from stackoverflow
+function getNextMonday() {
+  const d = new Date();
+  d.setDate(d.getDate() + (1 + 7 - d.getDay()) % 7);
+  return d
+}
+
+function getLogicalDate() {
+  const now = new Date()
+  const isWeekday = now.getDay() >= 1 && now.getDay() <= 5
+  const isSchoolOver = now.getHours() > 16
+
+  if (!isWeekday) {
+    return getNextMonday()
+  }
+
+  if (isSchoolOver) {
+    const tomorrow = new Date()
+    tomorrow.setDate(tomorrow.getDate() + 1)
+    return tomorrow
+  }
+
+  return now
+}
+
 export function load() {
-  let x = new Date()
+  const x = getLogicalDate()
 
   throw redirect(307, `/tt/${x.getFullYear()}-${x.getMonth() + 1}-${x.getDate()}`);
 }
