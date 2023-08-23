08eda2ee5cb670be27fe7d5d04191b91e676e287
niwla23 <46248939+niwla23@users.noreply.github.com>
2023-08-23 21:37:52 +0200

    docker

--- webviewer/svelte.config.js
+++ webviewer/svelte.config.js
@@ -1,5 +1,6 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-node'
+//import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
