3970c47f98bb24c926a445c8b769cff105d22f82
niwla23 <46248939+niwla23@users.noreply.github.com>
2024-01-15 17:29:06 +0100

    fix auth

--- webviewer/src/lib/clientAuth.ts
+++ webviewer/src/lib/clientAuth.ts
@@ -14,13 +14,16 @@ export async function loginPocketBase(pb: PocketBase) {
 export async function getAuthenticatedPocketBase() {
   const pb = new PocketBase('https://bbs-backend.noteqr.de');
   const looseCheckSucess = pb.authStore.model && pb.authStore.isValid
-  if (looseCheckSucess) {
-    try {
-      if (!pb.authStore.model) throw new Error()
-      await pb.collection("users").getOne(pb.authStore.model.id)
-    } catch (e) {
-      await loginPocketBase(pb)
-    }
+  try {
+    if (!looseCheckSucess) throw new Error()
+    if (!pb.authStore.model) throw new Error()
+    await pb.collection("users").getOne(pb.authStore.model.id)
+    console.log("success")
+  } catch (e) {
+    console.log("we want login")
+
+    await new Promise(r => setTimeout(r, 1000));
+    await loginPocketBase(pb)
   }
 
   return pb
