292adb3e7087302ab0bf938b4baee7f311d18b3b
niwla23 <46248939+niwla23@users.noreply.github.com>
2024-01-15 20:32:32 +0100

    add some labels

--- webviewer/src/routes/grades/EditSubjectNames.svelte
+++ webviewer/src/routes/grades/EditSubjectNames.svelte
@@ -18,26 +18,35 @@
 	}
 </script>
 
-<div class="fixed inset-0 w-full h-full flex justify-center items-center p-2 bg-darkest z-30">
+<div
+	class="absolute inset-0 w-full min-h-full h-fit flex justify-center items-center p-2 bg-darkest z-30"
+>
 	<div class="text-center p-4 max-w-lg w-full">
 		<h1 class="font-bold text-xl pb-4">Kurse bearbeiten</h1>
 
 		<div class="flex flex-col gap-2 w-full">
 			{#each subjectNameOptions as subject, subjectIndex}
-				{#if subject.length > 1}
-					<select
-						class="w-full rounded-md bg-dark p-2"
-						bind:value={gradeUserData[subjectIndex].nameOption}
-					>
-						{#each subject as option, optionIndex}
-							<option value={optionIndex}>
-								{option}
-							</option>
-						{/each}
-					</select>
-				{:else}
-					<p class="text-left p-2 bg-dark/80 rounded-md">{subject[0]}</p>
-				{/if}
+				<div class="flex items-center gap-2">
+					{#if subjectIndex < 5}
+						<span class="w-6 font-bold">P{subjectIndex + 1}</span>
+					{:else}
+						<span class="w-6">GK</span>
+					{/if}
+					{#if subject.length > 1}
+						<select
+							class="w-full rounded-md bg-dark p-2"
+							bind:value={gradeUserData[subjectIndex].nameOption}
+						>
+							{#each subject as option, optionIndex}
+								<option value={optionIndex}>
+									{option}
+								</option>
+							{/each}
+						</select>
+					{:else}
+						<p class="text-left p-2 bg-dark/80 rounded-md w-full">{subject[0]}</p>
+					{/if}
+				</div>
 			{/each}
 		</div>
 
