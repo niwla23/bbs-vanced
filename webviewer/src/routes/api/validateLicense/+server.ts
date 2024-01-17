ec67b799bfb4ea211f03b6b79c4f455eaaaf8057
niwla23 <46248939+niwla23@users.noreply.github.com>
2024-01-17 19:48:31 +0100

    move license keys to backend

--- webviewer/src/routes/api/validateLicense/+server.ts
+++ webviewer/src/routes/api/validateLicense/+server.ts
@@ -2,6 +2,7 @@
 import type { RequestHandler } from "@sveltejs/kit";
 import { env } from '$env/dynamic/private'
 import PocketBase from "pocketbase"
+import { pb, pbAuth } from "@/lib/serverHelpers";
 
 function sendJson(data: any, status?: number) {
   return new Response(JSON.stringify(data), { status: status || 200 })
@@ -23,28 +24,35 @@ export const GET: RequestHandler = async (event) => {
 
   if (!givenKey || givenKey == null) return sendJson({ "error": "No key given" }, 500)
   if (!accountId || accountId == null) return sendJson({ "error": "No accountId given" }, 500)
-  if (!env.LICENSE_SERVER_TOKEN) return sendJson({ "error": "no api token in env" }, 400)
+  // if (!env.LICENSE_SERVER_TOKEN) return sendJson({ "error": "no api token in env" }, 400)
 
   // check validity of key
-  const endpointUrl = new URL("https://app-easy-product-downloads.fr/api/get-license-key")
-  endpointUrl.searchParams.set("license_key", givenKey)
-  endpointUrl.searchParams.set("api_token", env.LICENSE_SERVER_TOKEN)
-
-  const result = await fetch(endpointUrl, { method: "POST" })
-
-  const data = await result.json()
-  if (data["status"] != "success") {
-    return sendJson({ "error": "No such key", valid: false }, 400)
+  await pbAuth()
+  const results = await pb.collection("legacy_keys").getFullList({ filter: (pb.filter("key = {:givenKey}", { givenKey })) })
+  if (results.length == 0) {
+    return sendJson({ "error": "No such key", valid: false }, 404)
   }
 
-  if (data["license_key"]["product"]["sku"] != "bbs-vanced-pro") {
-    return sendJson({ "error": "Key invalid for this product", "valid": false }, 401)
-  }
 
+  // const endpointUrl = new URL("https://app-easy-product-downloads.fr/api/get-license-key")
+  // endpointUrl.searchParams.set("license_key", givenKey)
+  // endpointUrl.searchParams.set("api_token", env.LICENSE_SERVER_TOKEN)
+  //
+  // const result = await fetch(endpointUrl, { method: "POST" })
+  //
+  // const data = await result.json()
+  // if (data["status"] != "success") {
+  //   return sendJson({ "error": "No such key", valid: false }, 400)
+  // }
+  //
+  // if (data["license_key"]["product"]["sku"] != "bbs-vanced-pro") {
+  //   return sendJson({ "error": "Key invalid for this product", "valid": false }, 401)
+  // }
+  //
   // key is technically valid, connect to backend
-  const pb = new PocketBase('https://bbs-backend.noteqr.de');
-  pb.autoCancellation(false);
-  await pb.collection('users').authWithPassword(env.PB_USER, env.PB_PASSWORD)
+  // const pb = new PocketBase('https://bbs-backend.noteqr.de');
+  // pb.autoCancellation(false);
+  // await pb.collection('users').authWithPassword(env.PB_USER, env.PB_PASSWORD)
 
 
   // make sure key has not been linked to another account yet
@@ -56,7 +64,7 @@ export const GET: RequestHandler = async (event) => {
   // log activation
   await pb.collection("activations").create({
     key: givenKey,
-    email: getOrderEmail(data),
+    // email: getOrderEmail(data),
     account: accountId,
     environment: env.NODE_ENV
   })
