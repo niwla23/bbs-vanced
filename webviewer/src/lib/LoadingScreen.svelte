377418ac06c3b4949ef9a2efec34d68e18d7a269
niwla23 <46248939+niwla23@users.noreply.github.com>
2024-02-11 19:24:22 +0100

    new loader

--- webviewer/src/lib/LoadingScreen.svelte
+++ webviewer/src/lib/LoadingScreen.svelte
@@ -8,10 +8,43 @@
 
 <div class="w-full h-full flex justify-center items-center" transition:animate>
 	<div class="text-center flex flex-col items-center">
-		<p class="text-[10rem] md:text-[14rem] animate-spin text-center w-min">{choosenEmoji}</p>
-		<p>Wird geladen ...</p>
-		<small class="text-xs text-muted">
+		<!-- <p class="text-[10rem] md:text-[14rem] animate-spin text-center w-min">{choosenEmoji}</p> -->
+		<div class="flex gap-4">
+			<div class="loading-dot" id="dot1" />
+			<div class="loading-dot" id="dot2" />
+			<div class="loading-dot" id="dot3" />
+		</div>
+		<small class="text-xs text-muted pt-4">
 			{choosenMotd}
 		</small>
 	</div>
 </div>
+
+<style>
+	@keyframes pulse {
+		0% {
+			transform: scale(1);
+		}
+		50% {
+			transform: scale(1.2);
+		}
+		70% {
+			transform: scale(1);
+		}
+		100% {
+			transform: scale(1);
+		}
+	}
+
+	.loading-dot {
+		animation: pulse 2s infinite ease-in-out;
+		@apply bg-dark w-12 h-12 rounded-full;
+	}
+
+	#dot2 {
+		animation-delay: 0.5s;
+	}
+	#dot3 {
+		animation-delay: 1s;
+	}
+</style>
---
+++ webviewer/src/routes/kitchensink/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+	import LoadingScreen from '@/lib/LoadingScreen.svelte';
+</script>
+
+<LoadingScreen />
