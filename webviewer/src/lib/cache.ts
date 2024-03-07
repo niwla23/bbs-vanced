0c69a899d1aeb4896b014a7b9c30d9a7e97a51a5
niwla23 <46248939+niwla23@users.noreply.github.com>
2024-03-07 09:26:05 +0100

    use shared redis

--- webviewer/src/lib/cache.ts
+++ webviewer/src/lib/cache.ts
@@ -1,15 +1,19 @@
 import { env } from '$env/dynamic/private';
 import { createClient } from 'redis';
 
-export async function createRedis() {
-  const redis = createClient({
-    url: `redis://${env.REDIS_HOST}:${env.REDIS_PORT}`
-  });
 
-  redis.on('error', err => console.log('[redis]', err));
-  await redis.connect()
+const redis = createClient({
+  url: `redis://${env.REDIS_HOST}:${env.REDIS_PORT}`
+});
+redis.on('error', err => console.log('[redis]', err));
+
+export async function createRedis() {
+  if (!redis.isOpen) {
+    await redis.connect()
+  }
   return redis
 }
+
 export function serialize(data: any): string {
   return JSON.stringify(data, (key, value) => {
     if (value instanceof Map) {
